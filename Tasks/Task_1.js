6a286d5fb6de187f0828c4e031d1e77f52cfbc13
Wandestes <gilua5568@gmail.com>
2024-12-10 13:52:11 +0200

    Update Task_1.js
    Prepare for Call back with mapWithCallback

--- Tasks/Task_1.js
+++ Tasks/Task_1.js
@@ -4,3 +4,25 @@
   * Prepare demo cases for the usage
 */
 // callback based async counterpart
+
+function mapWithCallback(array, callback, finalCallback) {
+    const results = [];
+    let processed = 0;
+
+    array.forEach((item, index) => {
+        callback(item, (error, result) => {
+                if (error) {
+                 console.error(error.message);
+                 results[index] = null;
+                } else {
+                    results[index] = result;
+                }
+
+            processed++;
+            if (processed === array.length) {
+                finalCallback(results);
+            }
+        });
+    });
+}
+
