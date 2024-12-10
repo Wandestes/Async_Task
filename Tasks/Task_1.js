818c65aa6d741474c0746718d091710c71bf576b
Wandestes <gilua5568@gmail.com>
2024-12-10 14:05:28 +0200

    Iterating over an array using forEach and processing the results of a callback function

--- Tasks/Task_1.js
+++ Tasks/Task_1.js
@@ -9,4 +9,19 @@ function mapWithCallback(array, callback, finalCallback) {
     const results = [];
     let processed = 0;
 
+    array.forEach((item, index) => {
+        callback(item, (error, result) => {
+                if (error) {
+                 console.error(error.message);
+                 results[index] = null;
+                } else {
+                    results[index] = result;
+                }
+
+            processed++;
+            if (processed === array.length) {
+                finalCallback(results);
+            }
+        });
+    });
 }
