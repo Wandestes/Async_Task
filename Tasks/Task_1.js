b52ceb962753a484ad3d41450c80d7776fc19c4d
Wandestes <gilua5568@gmail.com>
2024-12-10 13:59:30 +0200

    Create Task_1

---
+++ Tasks/Task_1.js
@@ -0,0 +1,7 @@
+/* Task 1
+  * Choose array fn (map/filter/filterMap/some/find/findIndex) (map)
+  * Prepare its callback based async counterpart
+  * Prepare demo cases for the usage
+*/
+// callback based async counterpart
+
