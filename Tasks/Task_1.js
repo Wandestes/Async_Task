54c997c41d61209ac0c85fed40314eb6353a415e
Wandestes <gilua5568@gmail.com>
2024-12-10 13:52:42 +0200

    Update Task_1.js
    Making demo case for the usage

--- Tasks/Task_1.js
+++ Tasks/Task_1.js
@@ -25,4 +25,24 @@ function mapWithCallback(array, callback, finalCallback) {
         });
     });
 }
+// demo case for the usage
+function demoFunc() {
+    const numbers = [1, 2, 3, 4, 5];
+
+    const tripleWithCallback = (num, callback) => {
+        setTimeout(() => {
+            if (Math.random() < 0.2) {
+                callback(`Failed to process ${num}`);
+            } else {
+                callback(null, num * 3);
+            }
+        }, Math.random() * 1000);
+    };
+
+    mapWithCallback(numbers, tripleWithCallback, (results) => {
+        console.log(results);
+    });
+}
+
+demoFunc();
 
