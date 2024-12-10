92ef45e97dc5f546c777cd76c490e271f346d81f
Wandestes <gilua5568@gmail.com>
2024-12-10 14:03:14 +0200

    Create mapWithCallback

---
+++ Tasks/Task_1.js
@@ -0,0 +1,12 @@
+/* Task 1
+  * Choose array fn (map/filter/filterMap/some/find/findIndex) (map)
+  * Prepare its callback based async counterpart
+  * Prepare demo cases for the usage
+*/
+// callback based async counterpart
+
+function mapWithCallback(array, callback, finalCallback) {
+    const results = [];
+    let processed = 0;
+
+}
